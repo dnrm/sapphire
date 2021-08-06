f760f141a29b5d79c997a714825da78e75d28807
Daniel Medina <daniel@medina.com>
2021-08-06 10:29:11 -0500

    :sparkles: Anonymous users can't see photos now!

--- components/Gallery.js
+++ components/Gallery.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Grid } from '@chakra-ui/react';
+import { Grid, Stack, Heading, Flex, Text } from '@chakra-ui/react';
 import Photo from './Photo';
+import { useSession } from 'next-auth/client';
 
 const Gallery = () => {
-    return (
+
+    const [session] = useSession()
+
+    return session ? (
         <Grid
             px={8}
             my={8}
@@ -36,6 +40,47 @@ const Gallery = () => {
             <Photo src="https://images.unsplash.com/photo-1611635473151-88f447ed3140"></Photo>
             <Photo src="https://images.unsplash.com/photo-1627759929352-e4ad6ff6d55e"></Photo>
         </Grid>
+    ) : (
+        <Stack
+            py={64}
+            m={8}
+            bgImage={'/unauthorised-bg.png'}
+            bgRepeat="no-repeat"
+            bgSize="cover"
+        >
+            <Flex
+                h={32}
+                w={'full'}
+                alignItems="center"
+                justifyContent="center"
+                opacity="1"
+            >
+                <Flex h={28} w={28} textColor="gray.600">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                    >
+                        <path
+                            fillRule="evenodd"
+                            d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                            clipRule="evenodd"
+                        />
+                    </svg>
+                </Flex>
+            </Flex>
+            <Heading
+                textColor="gray.600"
+                textAlign="center"
+                fontWeight="regular"
+            >
+                You are not Sofi or Dani!!!
+            </Heading>
+            <Text textColor="gray.600" textAlign="center" fontWeight="regular">
+                ...or you are not logged in :P
+            </Text>
+        </Stack>
     );
 };
 
