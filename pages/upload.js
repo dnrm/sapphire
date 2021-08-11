1057c8b04c2acec1808f24aa3b6a4e1721c8bc5b
Daniel Medina <daniel@medina.com>
2021-08-11 13:14:26 -0500

    :sparkles: Image to upload now appears in input.

--- pages/upload.js
+++ pages/upload.js
@@ -1,11 +1,14 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import Navbar from '../components/Navbar';
-import { Divider, Heading, Stack, Input, Flex } from '@chakra-ui/react';
+import { Divider, Heading, Stack, Input, Flex, Image, Button } from '@chakra-ui/react';
 import { useDropzone } from 'react-dropzone';
 
 const Upload = () => {
+    const [image, setImage] = useState();
+
     const onDrop = useCallback((acceptedFiles) => {
         console.log(acceptedFiles);
+        setImage(URL.createObjectURL(acceptedFiles[0]));
     }, []);
 
     const { getRootProps, getInputProps, isDragActive } = useDropzone({
@@ -21,10 +24,8 @@ const Upload = () => {
                     Upload a photo
                 </Heading>
                 <Flex
-                    px={25}
-                    py={50}
                     borderColor="gray.300"
-                    h={64}
+                    h={'70vh'}
                     w={'full'}
                     borderWidth={2}
                     {...getRootProps()}
@@ -32,25 +33,50 @@ const Upload = () => {
                     alignItems="center"
                     direction="column"
                     background={isDragActive ? 'telegram.100' : 'white'}
-                    textColor={isDragActive ? "gray.500" : "gray.500"}
+                    textColor={isDragActive ? 'gray.500' : 'gray.500'}
                     rounded="2xl"
                 >
                     <Input {...getInputProps()} display="none" />
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={1}
-                            d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-                        />
-                    </svg>
-                    <Heading fontWeight="regular">Upload image</Heading>
+                    {image ? (
+                        <>
+                            <Flex
+                                p={0}
+                                overflow="hidden"
+                                rounded="2xl"
+                                h={'100%'}
+                            >
+                                <Image
+                                    src={image}
+                                    alt="Image to be uploaded"
+                                    objectFit="cover"
+                                />
+                            </Flex>
+                        </>
+                    ) : (
+                        <Stack px={25} py={50} dir="column">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={1}
+                                    d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+                                />
+                            </svg>
+                            <Heading fontWeight="regular">Upload image</Heading>
+                        </Stack>
+                    )}
                 </Flex>
+                {image ? (
+                    <Stack pb={16}>
+                        <Divider my={8} />
+                        <Button variant="solid" colorScheme="teal">Upload</Button>
+                    </Stack>
+                ) : null}
             </Stack>
         </>
     );
