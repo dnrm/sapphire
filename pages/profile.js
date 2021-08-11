733f9a55b8f86a1af1e39e88842102e53303cd54
Daniel Medina <daniel@medina.com>
2021-08-11 10:55:32 -0500

    :sparkles: Added user data to profile page.

--- pages/profile.js
+++ pages/profile.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Head from 'next/head';
-import { Divider, Heading, Stack, Button } from '@chakra-ui/react';
+import { Divider, Heading, Stack, Button, Text, Image } from '@chakra-ui/react';
 import Navbar from '../components/Navbar';
-import { signOut } from 'next-auth/client'
+import { signOut, getSession } from 'next-auth/client';
 
-const profile = () => {
+const profile = ({ session }) => {
     return (
         <>
             <Head>
@@ -23,10 +23,41 @@ const profile = () => {
                 >
                     My Profile
                 </Heading>
-                <Button onClick={() => signOut({ callbackUrl: '/' })}>Sign Out</Button>
+                <Divider mb={8} />
+                <Stack id="user-data" py={8}>
+                    <Stack>
+                        {/* eslint-disable-next-line @next/next/no-img-element */}
+                        <Image src={session.user.image} alt="Profile image" w={24} rounded="full" borderColor="gray.200" borderWidth="4px" />
+                    </Stack>
+                    <Heading m={0} fontSize={{ base: '1em', md: '2em' }}>
+                        {session.user.name}
+                    </Heading>
+                    <Text
+                        m={0}
+                        fontSize={{ base: '0.8em', md: '1em' }}
+                        textColor={'gray.500'}
+                    >
+                        {session.user.email}
+                    </Text>
+                </Stack>
+                <Button bgColor="gray.200" onClick={() => signOut({ callbackUrl: '/' })}>
+                    Sign Out
+                </Button>
             </Stack>
         </>
     );
 };
 
 export default profile;
+
+export async function getServerSideProps(context) {
+    const session = await getSession(context);
+
+    if (session) {
+        return {
+            props: {
+                session,
+            },
+        };
+    }
+}
