4e64ad40ac3544b8c5da2c819d057dd4049d4c13
Daniel Medina <daniel@medina.com>
2021-09-07 15:50:45 -0500

    :bug: Fixed domains in next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['cdn.medina.dev', 'crystal-dnrm.s3.amazonaws.com']
+    domains: ['cdn.medina.dev', 'sapphire-dnrm.s3.amazonaws.com']
   }
 }
