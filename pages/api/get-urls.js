878d257a22bfa841bf7859d574b54781eda8076a
Daniel Medina <daniel@medina.com>
2021-09-13 10:18:39 -0500

    :construction: Started implementing cloudinary in api routes

--- pages/api/get-urls.js
+++ pages/api/get-urls.js
@@ -1,4 +1,12 @@
 import { getSession } from 'next-auth/client'
+import cloudinary from 'cloudinary'
+
+cloudinary.config({
+    cloud_name: 'dnrm',
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true
+})
 
 export default async function handler(req, res) {
     const session = await getSession({ req });
@@ -6,5 +14,10 @@ export default async function handler(req, res) {
         res.status(403).send({ message: 'unauthorised' })
     }
 
-    
+    // const photo = cloudinary.v2.search.expression(
+    //     'resource_type:image OR resource_type:video'
+    // ).sort_by('public_id', 'desc').execute().then(r => console.log(r))
+
+    res.send(200)
+
 }
