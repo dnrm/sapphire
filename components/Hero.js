d6f51c4c8649574678062193f6b1d3a6cf8964cf
Daniel Medina <daniel@medina.com>
2022-01-14 10:52:09 -0600

    :sparkles: Changed hero section to 1 photo

--- components/Hero.js
+++ components/Hero.js
@@ -11,14 +11,14 @@ const Hero = () => {
                     justifyContent="center"
                 >
                     <Avatar
-                        name="Sofia Hinojosa"
+                        name="Sofia Dani"
                         borderColor="gray.200"
                         borderRadius="full"
                         borderWidth={{ base: '4px', md: '8px' }}
                         h={{ base: '32', md: '56' }}
                         w={{ base: '32', md: '56' }}
                         m="2"
-                        src="https://res.cloudinary.com/dnrm/image/upload/c_fill,g_face,h_750,r_0,w_750/v1631149875/sofi.jpg"
+                        src="https://cdn.medina.dev/us.jpeg"
                     >
                         <AvatarBadge
                             borderColor="papayawhip"
@@ -30,7 +30,7 @@ const Hero = () => {
                                 xmlns="http://www.w3.org/2000/svg"
                                 className="h-5 w-5"
                                 viewBox="0 0 20 20"
-                                fill="currentColor"
+                                fill="white"
                             >
                                 <path
                                     fillRule="evenodd"
@@ -40,44 +40,7 @@ const Hero = () => {
                             </svg>
                         </AvatarBadge>
                     </Avatar>
-                    <Text>Sofi</Text>
-                </Flex>
-                <Flex
-                    flexDirection="column"
-                    alignItems="center"
-                    justifyContent="center"
-                >
-                    <Avatar
-                        name="Daniel Medina"
-                        borderColor="gray.200"
-                        borderRadius="full"
-                        borderWidth={{ base: '4px', md: '8px' }}
-                        h={{ base: '32', md: '56' }}
-                        w={{ base: '32', md: '56' }}
-                        m="2"
-                        src="https://res.cloudinary.com/dnrm/image/upload/c_fill,g_face,h_750,r_0,w_750/v1631149875/dani.jpg"
-                    >
-                        <AvatarBadge
-                            borderColor="papayawhip"
-                            bg="tomato"
-                            boxSize={{ base: '3em', md: '4em' }}
-                            p={{ base: '3', md: '4' }}
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-5 w-5"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                            >
-                                <path
-                                    fillRule="evenodd"
-                                    d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-                                    clipRule="evenodd"
-                                />
-                            </svg>
-                        </AvatarBadge>
-                    </Avatar>
-                    <Text>Dani</Text>
+                    <Text>Sofi &amp; Dani</Text>
                 </Flex>
             </Flex>
             <Text
